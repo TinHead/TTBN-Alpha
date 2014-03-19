accfde478498b14bdf76f4ce58f22b6c285d5077
Razvan <razvan.lixandru@gmail.com>
2014-03-19 10:14:57 +0200

    Latest stuff.

--- nano-code/src/sketch.ino
+++ nano-code/src/sketch.ino
@@ -1,3 +1,4 @@
+#include <Wire.h>
 
 #define EN0_PIN 5
 #define EN1_PIN 6
@@ -8,8 +9,20 @@
 #define M2_0_PIN 7
 #define M2_1_PIN 8
 
+struct packet {
+	bool complete;
+	int data_index;
+	int data[3];	
+};
+
+packet command;
+
 void setup()
 {
+	// I2c init address 2
+	Wire.begin(3);
+	Wire.onReceive(receive_data);
+
 	//debug serial
 	Serial.begin(9600);
 	//setup Enable pins
@@ -45,12 +58,98 @@ void loop()
 void get_battery()
 {
 	int raw = analogRead(0);
-	float val = map(raw, 0, 1023, 0.0, 50.0);
+	float val = fmap(raw, 0, 1023, 0.00, 5.00);
   	Serial.println(val);
+}
+
+void go_forward(int speed)
+{
+	digitalWrite(M1_0_PIN, HIGH);
+	digitalWrite(M1_1_PIN, LOW);
+	digitalWrite(M2_0_PIN, HIGH);
+	digitalWrite(M2_1_PIN, LOW);
+
+	analogWrite(EN0_PIN, speed);
+	analogWrite(EN1_PIN, speed);	
+	
+}
+
+void go_backward(int speed)
+{
+	digitalWrite(M1_1_PIN, HIGH);
+	digitalWrite(M1_0_PIN, LOW);
+	digitalWrite(M2_1_PIN, HIGH);
+	digitalWrite(M2_0_PIN, LOW);
+
+	analogWrite(EN0_PIN, speed);
+	analogWrite(EN1_PIN, speed);	
+}
+
+void go_left(int speed)
+{
+	digitalWrite(M1_0_PIN, HIGH);
+	digitalWrite(M1_1_PIN, LOW);
+	digitalWrite(M2_1_PIN, HIGH);
+	digitalWrite(M2_0_PIN, LOW);
+
+	analogWrite(EN0_PIN, speed);
+	analogWrite(EN1_PIN, speed);	
+	
+}
+
+void go_right(int speed)
+{
+	digitalWrite(M1_1_PIN, HIGH);
+	digitalWrite(M1_0_PIN, LOW);
+	digitalWrite(M2_0_PIN, HIGH);
+	digitalWrite(M2_0_PIN, LOW);
+
+	analogWrite(EN0_PIN, speed);
+	analogWrite(EN1_PIN, speed);	
+	
+}
+
+void stop()
+{
+	analogWrite(EN0_PIN, 0);
+	analogWrite(EN1_PIN, 0);
 }
 
 float fmap(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void execute_command(){
+	// called when a complete packet has been received 
+}
+
+void process(int data){
+	switch (data){
+		case 0:
+			command.complete=0;
+			command.data_index=0;
+			Serial.println("Command begin");
+		break;
+		case 2:
+			command.complete=1;
+			Serial.println("Command complete");			
+		default:
+			if (command.complete=0) {
+				Serial.println("Index is: ");
+				Serial.print(command.data_index);
+				Serial.println("Adding data:");
+				Serial.println(data);
+				command.data[command.data_index]=data;
+				command.data_index++;
+			}
+	}
+}
+
+void receive_data(int byteCount)
+{
+  //read the command from wire
+	int data = Wire.read(); 
+	process(data);
 }
 
