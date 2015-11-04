afb78f699956d0c46544fa316b448c282b63fcd0
Razvan Lixandru <lixandru@adobe.com>
2015-11-04 15:11:00 +0200

    Connected buttons to events

--- pi-code/tornado/static/app.js
+++ pi-code/tornado/static/app.js
@@ -2,11 +2,13 @@ window.onload = function() {
 
 // Get references to elements on the page.
 var form = document.getElementById('control-form');
+//var messageField = document.getElementById('message');
+//var messagesList = document.getElementById('messages');
 var socketStatus = document.getElementById('status');
 var closeBtn = document.getElementById('close');
 
 // Create a new WebSocket.
-var socket = new WebSocket('ws://localhost:8888/command');
+var socket = new WebSocket('ws://192.168.43.7:8888/command');
 
 // Handle any errors that occur.
 socket.onerror = function(error) {
@@ -24,7 +26,40 @@ socket.onerror = function(error) {
     socketStatus.innerHTML = 'Disconnected from WebSocket.';
     socketStatus.className = 'closed';
   };
-
+window.addEventListener('mousedown', function(event)
+    {
+        switch (event.target.name) {
+                case "left":
+                    socketStatus.innerHTML = event.target.name;
+                    socket.send('37');
+                break;
+                case "right":
+                    socket.send('39');
+                    socketStatus.innerHTML = event.target.name;
+                break;
+                case "forward":
+                    socket.send('38');
+                    socketStatus.innerHTML = event.target.name;
+                break;
+                case "back":
+                    socket.send('40');
+                    socketStatus.innerHTML = event.target.name;
+                break;
+                case "stop":
+                    socket.send('32');
+                    socketStatus.innerHTML = event.target.name;
+                break;
+                case "cam_up":
+                    socket.send('65');
+                    socketStatus.innerHTML = event.target.name;
+                break;
+                case "cam_down":
+                    socket.send('90');
+                    socketStatus.innerHTML = event.target.name;
+                break;
+        }
+    },
+false);
 window.addEventListener('keydown', function(event) {
   switch (event.keyCode) {
     case 37: // Left
@@ -34,7 +69,7 @@ window.addEventListener('keydown', function(event) {
 
     case 38: // Up
 	socket.send('38');
-	socketStatus.innerHTML = 'Up Key Pressed';	
+	socketStatus.innerHTML = 'Up Key Pressed';
     break;
 
     case 39: // Right
@@ -48,11 +83,19 @@ window.addEventListener('keydown', function(event) {
     break;
     case 32: //Space
         socket.send('32');
-        socketStatus.innerHTML = 'Space Key Pressed';	
-    break;	
+        socketStatus.innerHTML = 'Space Key Pressed';
+    break;
+    case 65: //a
+        socket.send('65');
+        socketStatus.innerHTML = 'A Key Pressed';
+    break;
+    case 90: //z
+        socket.send('90');
+        socketStatus.innerHTML = 'Z Key Pressed';
+    break;
+
   }
 }, false);
-
 
   // Close the WebSocket connection when the close button is clicked.
   closeBtn.onclick = function(e) {
--- pi-code/tornado/tornado_web.py
+++ pi-code/tornado/tornado_web.py
@@ -8,9 +8,12 @@ from time import sleep
 import os.path
 
 pigpio.start(port='8889')
-
+#
+cam_value = 1400
+#
 pigpio.set_servo_pulsewidth(7,0)
 pigpio.set_servo_pulsewidth(8,0)
+pigpio.set_servo_pulsewidth(24,cam_value)
 
 # This is our WebSocketHandler - it handles the messages
 # from the tornado server
@@ -19,50 +22,69 @@ class MainHandler(tornado.web.RequestHandler):
         self.render("index.html")
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    cam_value = 1400
     # the client connected
-	def open(self):
-		print "New client connected"
-		self.write_message("You are connected")
+    def open(self):
+        print "New client connected"
+        self.write_message("You are connected")
 
-	# the client sent the message
-	def on_message(self, message):
-		self.write_message(message)
-		if message == '38':
-			#up
-			print 'up'
-			pigpio.set_servo_pulsewidth(7,2500)
-			pigpio.set_servo_pulsewidth(8,500)
-		elif message == '40':
-			#down
-			print 'down'
-			pigpio.set_servo_pulsewidth(7,500)
-                        pigpio.set_servo_pulsewidth(8,2500)
-		elif message == '37':
-			#right
-			print 'right'
-			pigpio.set_servo_pulsewidth(7,500)
-                        pigpio.set_servo_pulsewidth(8,500)
-		elif message == '39':
-			#right
-			print 'left'
-			pigpio.set_servo_pulsewidth(7,2500)
-                        pigpio.set_servo_pulsewidth(8,2500)
-		elif message == '32':
-			#space	
-			print 'stop'
-			pigpio.set_servo_pulsewidth(7,0)
-                        pigpio.set_servo_pulsewidth(8,0)
-	# client disconnected
-	def on_close(self):
-		print "Client disconnected"
+    # the client sent the message
+    def on_message(self, message):
+        self.write_message(message)
+        if message == '38':
+            #up
+            print 'up'
+            pigpio.set_servo_pulsewidth(7,2500)
+            pigpio.set_servo_pulsewidth(8,500)
+        elif message == '40':
+            #down
+            print 'down'
+            pigpio.set_servo_pulsewidth(7,500)
+            pigpio.set_servo_pulsewidth(8,2500)
+        elif message == '37':
+            #right
+            print 'right'
+            pigpio.set_servo_pulsewidth(7,1400)
+            pigpio.set_servo_pulsewidth(8,1400)
+        elif message == '39':
+            #right
+            print 'left'
+            pigpio.set_servo_pulsewidth(7,1800)
+            pigpio.set_servo_pulsewidth(8,1800)
+        elif message == '32':
+            #space
+            print 'stop'
+            pigpio.set_servo_pulsewidth(7,0)
+            pigpio.set_servo_pulsewidth(8,0)
+        elif message == '65':
+            print 'a'
+            #print self.cam_value
+            if (self.cam_value < 1800):
+                pigpio.set_servo_pulsewidth(24,self.cam_value+100)
+                self.cam_value+=100
+            else:
+                self.cam_value=1800
+        elif message == '90':
+            print 'z'
+            #print self.cam_value
+            if (self.cam_value > 1300):
+                pigpio.set_servo_pulsewidth(24,self.cam_value+100)
+                self.cam_value-=100
+            else:
+                self.cam_value=1300
+
+
+    # client disconnected
+    def on_close(self):
+        print "Client disconnected"
 
 
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-	   (r"/command", WebSocketHandler),
-    	   (r"/",MainHandler),
+       (r"/command", WebSocketHandler),
+           (r"/",MainHandler),
         ]
         settings = dict(
           # cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
@@ -74,10 +96,9 @@ class Application(tornado.web.Application):
 
 # start the tornado server on port 8888
 def main():
-	app = Application()
-	app.listen(8888)
-	tornado.ioloop.IOLoop.instance().start()
+    app = Application()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-	main()    
-
+    main()
